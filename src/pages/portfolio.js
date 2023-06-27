ce45dc5ec63a0f2f08183aba833acf5fc17670a3
mani13211 <58296779+mani13211@users.noreply.github.com>
2023-06-27 09:44:02 +0530

    portfoliu

---
+++ src/pages/portfolio.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+function Portfolio() {
+  return (
+    <div className='container-xl m-auto p-5'>
+        <div className='row'>
+            <div className='col-lg-4 col-sm-6'>
+                <div className='cards card m-auto mb-3' >
+                    <div className='img-contaienr' >
+                        <img className="img-fluid"src="codeman1.jpg"/>
+                    </div>
+                    <h6>Sketck artist</h6>
+                    <p>lfkjsdhglksdhbflhbflksdjbhfljkbhfljdbhljh</p>
+                </div>
+            </div>
+            <div className='col-lg-4 col-sm-6'>
+                <div className='cards card m-auto mb-3' >
+                    <div className='img-contaienr' >
+                        <img className="img-fluid"src="codeman1.jpg"/>
+                    </div>
+                    <h6>Sketck artist</h6>
+                    <p>lfkjsdhglksdhbflhbflksdjbhfljkbhfljdbhljh</p>
+                </div>
+            </div>
+            <div className='col-lg-4 col-sm-6'>
+                <div className='cards card m-auto mb-3' >
+                    <div className='img-contaienr' >
+                        <img className="img-fluid"src="codeman1.jpg"/>
+                    </div>
+                    <h6>Sketck artist</h6>
+                    <p>lfkjsdhglksdhbflhbflksdjbhfljkbhfljdbhljh</p>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Portfolio
