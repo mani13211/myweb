7f35d84d7db583bcf4ce50a048d6e6c0fb6b10d2
mani13211 <58296779+mani13211@users.noreply.github.com>
2023-07-04 22:46:51 +0530

    conatct chnges

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,9 +4,12 @@ import Header from './components/header'
 import Footer from './components/footer'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../../node_modules/bootstrap/dist/css/bootstrap.css"
 import '@/styles/css/globals.css'
 import "../styles/css/index.css"
+import "../styles/css/contact.css"
+
+
 //import "../../node_modules/bootstrap/dist/js/bootstrap.min.js"
 
 
